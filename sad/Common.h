dddbe1f1f9bf361ec83c09d0afc716a72f9d8084
arun_forcepoint_laptop <arunjoseph3007@gmail.com>
2025-09-20 11:44:23 +0530

    some utility fucntions

--- sad/Common.h
+++ sad/Common.h
@@ -5,3 +5,13 @@
 typedef std::vector<std::string> TextBuffer;
 
 TextBuffer splitString(const std::string& text, const char& delimiter = '\n');
+
+void todo(std::string name);
+
+void unreachable(std::string name);
+
+void hack(std::string name);
+
+
+#define TODO() todo(__FUNCTION__)
+#define UNREACHABLE() unreachable(__FUNCTION__)
